a994807ca414db8fc24ea1ed8a8fb043f533eeae
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-08 23:19:21 +0100

    restaurant-card

--- features/restaurants/components/restaurant-info/restaurant-info.component.jsx
+++ features/restaurants/components/restaurant-info/restaurant-info.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text , View , StyleSheet } from 'react-native';
+import { Card }  from 'react-native-paper';
 
 export const RestaurantInfo = ({restaurant}) => {
   const {
@@ -12,6 +13,23 @@ export const RestaurantInfo = ({restaurant}) => {
     isClosedTemporarily
   } = restaurant;
   return(
-    <Text>{name}</Text>
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source= {{ uri : photos[0]}} />
+      <Text style={styles.title}>{name}</Text>
+    </Card>
   );
 }
+
+
+const styles = StyleSheet.create({
+  card : {
+    backgroundColor : 'white'
+  },
+  cover : {
+    padding : 20,
+    backgroundColor : 'white'
+  },
+  title : {
+    padding : 16
+  }
+})
