4cb9a0cfc4959235bfcdbd0d696e23fe94f3cb10
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-08 23:15:33 +0100

    distruct our restaurant-info

--- features/restaurants/components/restaurant-info/restaurant-info.component.jsx
+++ features/restaurants/components/restaurant-info/restaurant-info.component.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Text , View , StyleSheet } from 'react-native';
 
-export const RestaurantInfo = () => {
+export const RestaurantInfo = ({restaurant}) => {
+  const {
+    name = 'Some Restaurant',
+    icon,
+    photos,
+    adress = '100 Hay Nahda 1 Gr Al Ahd B03',
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily
+  } = restaurant;
   return(
-    <Text>Restaurant Info</Text>
+    <Text>{name}</Text>
   );
 }
