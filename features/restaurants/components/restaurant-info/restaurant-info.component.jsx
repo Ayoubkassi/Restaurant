fff1b6abc1a2e3db36650b53bbd95bca1fde310a
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-09 07:21:17 +0100

    add styled-components

--- features/restaurants/components/restaurant-info/restaurant-info.component.jsx
+++ features/restaurants/components/restaurant-info/restaurant-info.component.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Text , View , StyleSheet } from 'react-native';
 import { Card }  from 'react-native-paper';
+import styled from 'styled-components/native';
+
+const Title = styled.Text`
+  padding : 16px;
+`;
 
 export const RestaurantInfoCard = ({restaurant}) => {
   const {
@@ -15,7 +20,7 @@ export const RestaurantInfoCard = ({restaurant}) => {
   return(
     <Card elevation={5} style={styles.card}>
       <Card.Cover key={name} style={styles.cover} source= {{ uri : photos[0]}} />
-      <Text style={styles.title}>{name}</Text>
+      <Title>{name}</Title>
     </Card>
   );
 }
@@ -28,8 +33,5 @@ const styles = StyleSheet.create({
   cover : {
     padding : 20,
     backgroundColor : 'white'
-  },
-  title : {
-    padding : 16
   }
 })
