8c3b02adba713d7fd9490c66ccd0967632e4d251
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-09 09:42:24 +0100

    Clearnup our theme by applying styled-components

--- features/restaurants/components/restaurant-info/restaurant-info.component.jsx
+++ features/restaurants/components/restaurant-info/restaurant-info.component.jsx
@@ -4,17 +4,17 @@ import { Card }  from 'react-native-paper';
 import styled from 'styled-components/native';
 
 const Title = styled.Text`
-  padding : 16px;
+  padding : ${(props) => props.theme.space[3]};
   color : ${props => props.theme.colors.ui.primary};
 `;
 
 const RestaurantCard = styled(Card)`
-  background-color : 'white';
+  background-color : ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-  padding : 20px;
-  background-color : 'white';
+  padding :  ${(props) => props.theme.space[3]};
+  background-color : ${(props) => props.theme.colors.bg.primary};
 `
 
 
@@ -28,6 +28,7 @@ export const RestaurantInfoCard = ({restaurant}) => {
     rating = 4,
     isClosedTemporarily
   } = restaurant;
+
   return(
     <RestaurantCard elevation={5} >
       <RestaurantCardCover key={name}  source= {{ uri : photos[0]}} />
