ece098e36e8fceeac02e13101b8b5638c44ed3e0
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-10 16:29:29 +0100

    adding rating with svg and adress

--- features/restaurants/components/restaurant-info/restaurant-info.component.jsx
+++ features/restaurants/components/restaurant-info/restaurant-info.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Text , View , StyleSheet } from 'react-native';
 import { Card }  from 'react-native-paper';
 import styled from 'styled-components/native';
+import { SvgXml } from 'react-native-svg';
+import star from '../../../../assets/star';
 
 const Title = styled.Text`
-  padding : ${(props) => props.theme.space[3]};
   color : ${props => props.theme.colors.ui.primary};
-  font-familly : ${(props) => props.theme.fonts.body};
+  font-familly : ${(props) => props.theme.fonts.heading};
+  font-size : ${(props) => props.theme.fontSizes.body};
 `;
 
 const RestaurantCard = styled(Card)`
@@ -18,6 +20,20 @@ const RestaurantCardCover = styled(Card.Cover)`
   background-color : ${(props) => props.theme.colors.bg.primary};
 `
 
+const Info = styled.View`
+  padding : ${(props) => props.theme.space[3]};
+`;
+
+const Adress = styled.Text`
+  font-familly : ${(props) => props.theme.fonts.body};
+  font-size : ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Rating = styled.View`
+  flex-direction : row;
+  padding-top : ${(props) => props.theme.space[2]};
+  padding-bottom : ${(props) => props.theme.space[2]};
+`;
 
 export const RestaurantInfoCard = ({restaurant}) => {
   const {
@@ -30,10 +46,19 @@ export const RestaurantInfoCard = ({restaurant}) => {
     isClosedTemporarily
   } = restaurant;
 
+  const ratingARray = Array.from(new Array(Math.floor(rating)));
   return(
     <RestaurantCard elevation={5} >
       <RestaurantCardCover key={name}  source= {{ uri : photos[0]}} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Rating>
+        { rattinArray.map(() => (
+          <SvgXml  xml={star} width={20} height={20} />
+        ))}
+        </Rating>
+        <Adress>{adress}</Adress>
+      </Info>
     </RestaurantCard>
   );
 }
