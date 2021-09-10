fbf00806567682d6497d8a488357f6c2a97d11d6
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-10 16:54:59 +0100

    adding Spacer and finishing Card

--- features/restaurants/components/restaurant-info/restaurant-info.component.jsx
+++ features/restaurants/components/restaurant-info/restaurant-info.component.jsx
@@ -4,6 +4,7 @@ import { Card }  from 'react-native-paper';
 import styled from 'styled-components/native';
 import { SvgXml } from 'react-native-svg';
 import star from '../../../../assets/star';
+import open from '../../../../assets/open';
 
 const Title = styled.Text`
   color : ${props => props.theme.colors.ui.primary};
@@ -35,15 +36,30 @@ const Rating = styled.View`
   padding-bottom : ${(props) => props.theme.space[2]};
 `;
 
+const SectionEnd = styled.View`
+  flex : 1;
+  flex-direction : row,
+  justify-content : flex-end;
+`;
+
+const Section = styled.View`
+  flex-direction : row;
+  align-items : center;
+`;
+
+const Open = styled(SvgXml)`
+  flex-direction : row;
+`;
+
 export const RestaurantInfoCard = ({restaurant}) => {
   const {
     name = 'Some Restaurant',
-    icon,
-    photos,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [ "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",],
     adress = '100 Hay Nahda 1 Gr Al Ahd B03',
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily
+    isClosedTemporarily = true
   } = restaurant;
 
   const ratingARray = Array.from(new Array(Math.floor(rating)));
@@ -52,11 +68,26 @@ export const RestaurantInfoCard = ({restaurant}) => {
       <RestaurantCardCover key={name}  source= {{ uri : photos[0]}} />
       <Info>
         <Title>{name}</Title>
+        <Section>
         <Rating>
-        { rattinArray.map(() => (
-          <SvgXml  xml={star} width={20} height={20} />
-        ))}
-        </Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
         <Adress>{adress}</Adress>
       </Info>
     </RestaurantCard>
