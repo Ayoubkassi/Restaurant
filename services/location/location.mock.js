2f0fc35dbec660d4962d5bad568425d431d54d0e
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-12 16:37:49 +0100

    Setting up GeoCoding Service (as Google Map)

---
+++ services/location/location.mock.js
@@ -0,0 +1,25 @@
+export const locations = {
+  antwerp : {
+    results : [
+      {
+        geometry : {
+          loaction : {
+            lng : 4.402464,
+            lat : 51.219448,
+          },
+          viewport : {
+            northeast : {
+              lat : 51.2145994309,
+              lng : 4.43611767812,
+            },
+            southwest : {
+              lat : 51.2145994309,
+              lng : 4.43611767812,
+            },
+
+          }
+        }
+      }
+    ]
+  }
+}
---
+++ services/location/location.service.js
@@ -0,0 +1,19 @@
+import camelize from 'camelize';
+import { locations } from './location.mock';
+
+export const locationRequest = (searchTerm) => {
+  return new Promise((reslove , reject) => {
+    const locationMock = locations[searchTerm];
+    if(!locationMock) reject("Not found");
+
+    resolve(locationMock);
+  });
+};
+
+export const locationTransform = (result) => {
+  const formattedResponse = camelize(result);
+  const { geometry = {} } = formattedResponse.results[0];
+  const { lat , lng } = geometry.location;
+
+  return {lat , lng};
+};
