c30e86b9bb4f145dcffac52ad7afa0914142fc94
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-12 16:24:09 +0100

    Add Activity Indicator (other solution hoc pattern)

--- features/restaurants/screens/restaurant-screen/restaurant-screen.jsx
+++ features/restaurants/screens/restaurant-screen/restaurant-screen.jsx
@@ -8,6 +8,9 @@ import { Spacer } from '../../../../components/spacer/spacer.component';
 import { SafeArea } from '../../../../components/safe-area/safe-area.component';
 import { useContext } from 'react';
 import { RestaurantsContext } from '../../../../services/restaurants/restaurants.context';
+import { ActivityIndicator , Colors } from 'react-native-paper';
+
+
 
 const SearchContainer = styled.View`
   padding : ${(props) => props.theme.space[3]};
@@ -22,11 +25,34 @@ const RestaurantList = styled(FlatList).attrs({
  /*using this one up if we want to remove flex 1 from the SafeArea */
 `;
 
+const LoadingContainer = styled.View`
+  position : absolute;
+  top : 50%;
+  left : 50%;
+`;
+
+const Loading = styled(ActivityIndicator)`
+  margin-left : -25px;
+`;
+
 
 export const RestaurantsScreen = () => {
   const { restaurants, isLoading , error } = useContext(RestaurantsContext);
+
+
   return (
     <SafeArea>
+    {
+      isLoading && (
+        <LoadingContainer>
+          <Loading
+            size={50}
+            animating = {true}
+            color = {Colors.blue300}
+          />
+        </LoadingContainer>
+      )
+    }
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
