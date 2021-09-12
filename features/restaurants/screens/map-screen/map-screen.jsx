8d4c796de98ed8e8db7ad1ba9e7d9f18bd2e92f7
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-12 23:40:47 +0100

    Install and Integrate React Native Maps

--- features/restaurants/screens/map-screen/map-screen.jsx
+++ features/restaurants/screens/map-screen/map-screen.jsx
@@ -1,13 +1,19 @@
-import React from react;
+import React  , {useState , useEffect , useContext} from 'react';
 import { View , Text } from 'react-native';
 import { SafeArea } from '../../../../components/safe-area/safe-area.component';
+import MapView from 'react-native-maps';
+import styled from 'styled-components/native';
+
+
+export const MapContainer = styled(MapView)`
+  height : 00%;
+`;
+
 
 export const MapScreen = () => {
   return (
-    <SafeArea>
-      <View>
-        <Text>Map Screen</Text>
-      </View>
-    </SafeArea>
+
+        <MapContainer />
+
   );
 }
--- features/restaurants/screens/setting-screen/setting-screen.jsx
+++ features/restaurants/screens/setting-screen/setting-screen.jsx
@@ -1,4 +1,4 @@
-import React from react;
+import React from 'react';
 import { View , Text } from 'react-native';
 import { SafeArea } from '../../../../components/safe-area/safe-area.component';
 
