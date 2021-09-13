4ce9d7c23a53392ae0024b129bd4cacb6bdddf63
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-13 11:58:38 +0100

    Setting The Map Region

--- features/maps/screens/map-screen/map-screen.jsx
+++ features/maps/screens/map-screen/map-screen.jsx
@@ -5,6 +5,9 @@ import MapView from 'react-native-maps';
 import styled from 'styled-components/native';
 import { Search } from '../../components/search.component';
 
+import { LocationContext } from '../../../../services/loaction/location.context';
+import { RestaurantsContext } from '../../../../services/restaurants/restaurants.context';
+
 export const Map = styled(MapView)`
   height : 100%;
   width : 100%;
@@ -12,10 +15,40 @@ export const Map = styled(MapView)`
 
 
 export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+  const [ latDelta , setLatDelta] = useState(0);
+  const { viewport , lat , lng } = location;
+
+  useEffect(()=> {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    const latDelta = northeastLat è southwestLat;
+    setLatDelta(latDelta);
+  },[location , viewport]);
   return (
     <>
         <Search />
-        <Map />
+        <Map
+          region = {{
+            latitude : lat,
+            longitude : lng,
+            latitudeDelta : latDelta,
+            longitudeDelta : 0.02,
+          }}
+        >
+          { restaurants.map((restaurant)=> {
+            return <MapView.Marker
+                    coordinate = {{
+                      longitude : ,
+                      latitude :
+                    }}
+                  >
+
+                  </MapView.Marker>;
+          }) }
+        </Map>
     </>
   );
 }
