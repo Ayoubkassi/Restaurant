10db62fbd85de4faf0933492d18621dfc987b85c
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-13 20:57:23 +0100

    Account Screen Button And Navigation

--- features/account/screens/account.screen.js
+++ features/account/screens/account.screen.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { AccountBackground,
-         AccountCover
+         AccountCover,
+         AccountContainer,
+        AuthButton
   } from './account.styles';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
-
-export const AccountScreen = () => {
+export const AccountScreen = ({ navigation }) => {
   return(
     <AccountBackground>
        <AccountCover />
+       <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          onPress={()=> navigation.navigate('Login')}
+        >Login</AuthButton>
+
+        <Spacer size="large">
+        <AuthButton
+          icon="lock-open-outline"
+          onPress={()=> navigation.navigate('Register')}
+        >Register</AuthButton>
+        </Spacer>
+       </AccountContainer>
     </AccountBackground>
   );
 }
--- features/account/screens/account.styles.js
+++ features/account/screens/account.styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components/native';
 import { View ,  ImageBackground } from 'react-native';
+import { colors } from '../../../infrastructure/theme/colors';
+import { Button } from 'react-native-paper';
+
+
 export const AccountBackground = styled.ImageBackground.attrs({
   source : require('../../../../assets/home_bg.jpg'),
 })`
@@ -16,4 +20,16 @@ export const AccountCover = styled.View`
   height : 100%;
   background-color : rgba(255,255,255,0.3);
 
+`;
+
+export const AccountContainer = styled.View`
+  background-color : rgba(255,255,255,0.7);
+  padding : ${(props) => props.theme.space[4]};
+  margin-top : ${(props)=> props.theme.space[2]};
+`;
+
+export const AuthButton = styled(Button).attrs({
+  color : colors.brand.primary,
+})`
+  padding : ${(props) => props.theme.space[2]};
 `;
