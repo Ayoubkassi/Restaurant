6635b686cc6497476e946d1cc3c08b11d729721d
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-15 22:25:54 +0100

    Add Expo Camera functionaity

--- features/settings/screens/camera.screen.jsx
+++ features/settings/screens/camera.screen.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React , { useRef , useState , useEffect} from 'react';
+import { Camera } from 'expo-camera';
+import styled from 'styled-components/native';
+import { View } from 'react-native';
+import { Text } from '../../components/typography/text.component';
+
+const ProfileCamera = styled(Camera)`
+  width : 100%;
+  height : 100%;
+`;
+
 
 export const CameraScreen = () => {
+  const [ hasPermission , setHasPermission ] = useState(null);
+  const cameraRef = useRef();
+
+  useeffect(() => {
+      (async () => {
+        const { status } = await Camera.requestPermissionAsync();
+        setHasPermission(status === 'granted');
+      })();
+  }, []);
+
+  if(hasPermission === null){
+    return <View />;
+  }
+
+  if(hasPermission === false){
+    return
+    <Text>No Access to Camera</Text>
+  }
   return(
-    
+      <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera )}
+      type={Camera.Constants.TYpe.front}
+      >
+
+      </ProfileCamera>
   );
 }
