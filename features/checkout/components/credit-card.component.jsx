a5d99f816b56f9c1d83facc9edc0bc24d7460184
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-17 16:28:10 +0100

    Get First Stripe Token

--- features/checkout/components/credit-card.component.jsx
+++ features/checkout/components/credit-card.component.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { LiteCreditCardInput } from 'react-native-credit-card-input';
+import createStripe from 'stripe-client';
+
+const stripe = createStripe("pk_test_51I0jtGF0TFxEW4qmG3adUE89QtZzhPjD23Dwj77euxrHMcj07IauercWEe7PIGFbo2K9CH4rqOE8neRR9OwlXKKY00Il7jZKDL
+");
 
 export const CreditCardInput = () => {
   const onChange = (formData) => {
     const { values , status } = formData;
     const isIncomplete = Object.values(status).includes("incomplete");
+    const card = {
+      number: '4242424242424242',
+      exp_month : '02',
+      exp_year:'24',
+      cvc: '244',
+      name  : 'Kassi',
+    }
+  const info = await  stripe.createToken({ card })
   };
 
   return(
